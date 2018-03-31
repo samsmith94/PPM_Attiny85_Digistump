c3995da56f3241289ec290101dc16add89ba208e
samsmith94 <sunikovacs@gmail.com>
2018-03-31 12:25:49 +0200

    working solution added

--- Arduino/PPMDigispark/PPMDigispark.ino
+++ Arduino/PPMDigispark/PPMDigispark.ino
@@ -1,46 +1,52 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define HIGH_PULSE_LENGTH (500>>4)
+#define PERIOD  (20100>>4)
+#define NUM_OF_CHANNELS 8
+
+const uint16_t pwm[NUM_OF_CHANNELS] = {1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700};
+
 volatile uint16_t matches = 0;
 volatile uint16_t desired_low_match = 0;
 volatile uint8_t index = 0;
-const uint16_t pwm[2] = {1000, 2000};
 
-volatile uint16_t eddigi_length = 0;
-//magas jel
+volatile uint16_t len_of_actual_frame = 0;
+
+//high pulse
 ISR(TIMER0_COMPA_vect)
 {
-  if (matches < 31) {
+  if (matches < HIGH_PULSE_LENGTH) {
     matches++;
   } else {
     matches = 0;
     stop_timer_0();
 
-    if (index == 0) {
-      desired_low_match = (pwm[index] >> 4) - (500 >> 4);
-      eddigi_length += desired_low_match;
-      index++; 
-    } else if (index == 1) {
-      desired_low_match = (pwm[index] >> 4) - (500 >> 4);
-      eddigi_length += desired_low_match;
-
-      index++;
-      
-      
-    } else if (index == 2) {
-      desired_low_match = (20000 >> 4) - (eddigi_length + (500 >> 4));
-      eddigi_length = 0;
-      index = 0;
-    }
-    
+    switch(index) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+        desired_low_match = (pwm[index] >> 4) - (HIGH_PULSE_LENGTH);
+        len_of_actual_frame += desired_low_match + HIGH_PULSE_LENGTH;
+        index++;
+        break;
+      case 8:
+        desired_low_match = PERIOD - (len_of_actual_frame + HIGH_PULSE_LENGTH);
+        len_of_actual_frame = 0;
+        index = 0;
+        break;
+    }   
     set_gpio_low();
     start_timer_1();
   }
 }
 
-
-
-//ez az alacsony jel
+//low
 ISR(TIMER1_COMPA_vect)
 {
   if (matches < desired_low_match) {
@@ -52,7 +58,6 @@ ISR(TIMER1_COMPA_vect)
     start_timer_0();
   }
 }
-
 
 void init_gpio(void)
 {
